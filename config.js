730d6b06c70b11059d70d62a071d8edd4d93356e
petersorensen <phojholt@aol.com>
2017-12-14 20:34:20 +0100

    forsoeg 2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://peter:peter12@ds227045.mlab.com:27045/railscapstone',
-//    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+//    'mongoUrl': 'mongodb://peter:peter12@ds227045.mlab.com:27045/railscapstone',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientID: '161395007609379',
         clientSecret: 'de716879d38cecac5c23f524afb9b617',
